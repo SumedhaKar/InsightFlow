0adb0253c166a8685cc85c24e0a31552aaaf8cbe
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-06-05 15:24:32 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/notisetting/notisetting_model.dart
@@ -0,0 +1,30 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'notisetting_widget.dart' show NotisettingWidget;
+import 'package:flutter/material.dart';
+
+class NotisettingModel extends FlutterFlowModel<NotisettingWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for Switch widget.
+  bool? switchValue1;
+  // State field(s) for Switch widget.
+  bool? switchValue2;
+  // State field(s) for Switch widget.
+  bool? switchValue3;
+  // State field(s) for Switch widget.
+  bool? switchValue4;
+  // State field(s) for Switch widget.
+  bool? switchValue5;
+  // State field(s) for Switch widget.
+  bool? switchValue6;
+  // State field(s) for Switch widget.
+  bool? switchValue7;
+  // State field(s) for Switch widget.
+  bool? switchValue8;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
